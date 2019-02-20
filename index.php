f39787752c838870639444309eaf1986ef36f2e3
Roman Losev <pilotaz@gmail.com>
2019-02-20 11:22:18 +0200

    desktop on clickdown trigger readyClick

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <html>
 <head>
-	<link href="pub/css/style.css" rel="stylesheet" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<link href="pub/css/style.css?v=022011" rel="stylesheet" />
 	<title>Reaction time</title>
 	<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
@@ -13,13 +15,13 @@
 				<div v-if="!isReady" @click="toearlyClick">
 					Wait for a green color
 				</div>
-				<div v-else @click="readyClick">
+				<div v-else @mousedown="readyClick">
 					Click!
 				</div>
 			</div>
 		</div>
-		<div v-if="showStats" @click="resetStats">Last: {{ last }} Avg: {{average}}</div>
+		<div v-if="showStats" @mousedown="resetStats">Last: {{ last }} Avg: {{average}}</div>
 	</div>
 </body>
-<script src="pub/js/script.js"></script>
+<script src="pub/js/script.js?v=022011"></script>
 </html>
